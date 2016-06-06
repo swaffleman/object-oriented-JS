e33be93c7e2c47f61194144986cb00c6a5ddbe2c
Steven Kinnamon <swaffleman@hotmail.com>
2016-06-06 01:29:49 -0400

    The quiz app I tried to do...

---
+++ ObjectOrientedQuizApp/app.js
@@ -0,0 +1,9 @@
+
+var questions = [
+    new Question("Who was the first President of the United States?", ["George Washington", "Thomas Jefferson"], "George Washington"),
+    new Question("What is the answer to the Ultimate Question of Life, the Universe, and Everything?", ["Pi", "42"], "42")
+];
+
+var quiz = new Quiz(questions);
+
+QuizUI.displayNext();
